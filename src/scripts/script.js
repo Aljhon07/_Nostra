7d46af5fc644d693395ef5f2a59dccdd412c7545
Aljhon07 <aljhon.dev07@gmail.com>
2024-06-10 11:05:39 +0800

    remove auth

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,13 +1,12 @@
 import { initGlobal } from "./core/global.js"
 import  {initPageNavigator} from "./core/pageNavigator.js"
-
+let initialLoad = true;
 
 window.addEventListener('DOMContentLoaded', () => {
-    const isAuth = JSON.parse(localStorage.getItem('auth'))
-    console.log(isAuth)
-    if(!isAuth) {
-        window.location.href = "/sign-in.html"
-    }
+    // const isAuth = JSON.parse(localStorage.getItem('auth'))
+    // if(!isAuth) {
+    //     window.location.href = "/sign-in.html"
+    // }
     window.location.href = "#/"
     initGlobal()
     initPageNavigator()
